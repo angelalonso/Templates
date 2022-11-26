8701fff4438e7761590e360a78ab85b4c40d987b
Angel Alonso <alonsofonseca.angel@gmail.com>
2022-11-26 11:53:25 +0100

    logs with colors

--- shell/logs.sh
+++ shell/logs.sh
@@ -4,24 +4,29 @@ function show_log {
   TSTAMP=$(date "+%Y-%m-%d %H:%M:%S")
   case $1 in
     "debug"|"d")
-      echo "[$TSTAMP][DEBUG] - $2"
+      #echo "[$TSTAMP][DEBUG] - $2"
+      printf '\e[35m%s\n\e[0m' "[$TSTAMP][DEBUG] - $2"
       ;;
     "info"|"i")
-      echo "[$TSTAMP][INFO] - $2"
+      #echo "[$TSTAMP][INFO] - $2"
+      printf '\e[32m%s\n\e[0m' "[$TSTAMP][INFO] - $2"
       ;;
     "warn"|"w")
-      echo "[$TSTAMP][WARN] - $2"
+      #echo "[$TSTAMP][WARN] - $2"
+      printf '\e[33m%s\n\e[0m' "[$TSTAMP][WARN] - $2"
       ;;
     "error"|"err"|"e")
-      echo "[$TSTAMP][ERROR] - $2"
+      #echo "[$TSTAMP][ERROR] - $2"
+      printf '\e[31m%s\n\e[0m' "[$TSTAMP][ERROR] - $2"
       ;;
     *)
-      echo "[$TSTAMP][DEBUG] - Wrong Logging mode"
+      #echo "[$TSTAMP][DEBUG] - Wrong Logging mode"
+      printf '\e[35m%s\n\e[0m' "[$TSTAMP][DEBUG] - $2"
       exit 2
       ;;
   esac
-
 }
+
 
 show_log debug "test one, two, three"
 show_log info "test one, two, three"
